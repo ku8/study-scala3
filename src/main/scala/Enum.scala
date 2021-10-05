30e1e686ff822f6710ce27eb824b5b48c6287e1d
gyutan8 <keiyu.harada@septeni-original.co.jp>
2021-10-05 10:28:41 +0900

    enum

---
+++ src/main/scala/Enum.scala
@@ -0,0 +1,14 @@
+
+/*
+  列挙型
+    引数を持たせることも可能
+*/
+enum DayOfWeek {
+  case Monday extends DayOfWeek
+  case Tuesday extends DayOfWeek
+  case Wednesday extends DayOfWeek
+  case Thursday extends DayOfWeek
+  case Friday extends DayOfWeek
+  case Saturday extends DayOfWeek
+  case Sunday extends DayOfWeek
+}
